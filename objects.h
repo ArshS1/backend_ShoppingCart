d135a3bad41047e5374ab8f796c40e121d61117e
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2022-07-16 13:15:30 -0400

    add header file

---
+++ objects.h
@@ -0,0 +1,113 @@
+#include <string>
+#include <unordered_map>
+
+// Product Object
+class Product
+{
+private:
+    int id;
+    std::string name;
+    double price;
+
+public:
+    Product() {}
+    Product(int item_id, std::string item_name, double item_price)
+    {
+        id = item_id;
+        this->name = item_name;
+        this->price = item_price;
+    }
+    std::string getItemName()
+    {
+        return name;
+    }
+
+    std::string getDisplayName()
+    {
+        return name + " : $" + std::to_string(price).substr(0, 4) + "\n";
+    }
+    char getFirstLetter()
+    {
+        return name[0];
+    }
+    friend class Item;
+    friend class Cart;
+};
+
+class Item
+{
+    const Product x;
+    int count = 0;
+
+public:
+    Item() {}
+    Item(Product temp, int y) : x(temp), count(y) {}
+
+    double getItemPrice()
+    {
+        return count * x.price;
+    }
+
+    std::string getItemsInfo()
+    {
+        return std::to_string(count) + " x " + x.name + "\n";
+    }
+    friend class Cart;
+};
+
+// Cart Object
+class Cart
+{
+private:
+    std::unordered_map<int, Item> userCart;
+
+public:
+    void addProduct(Product prod)
+    {
+
+        // Check if item is already in hashmap
+        if (userCart.count(prod.id) == 0)
+        {
+            int val = 1;
+            Item newProduct(prod, val);
+            userCart.insert(std::pair<int, Item>(prod.id, newProduct));
+        }
+        else
+        {
+            userCart[prod.id].count++;
+        }
+    }
+
+    std::string viewCart()
+    {
+        std::string itemsCarted = "";
+        if (userCart.empty())
+        {
+            itemsCarted += "\nYour cart is empty.\n";
+        }
+        else
+        {
+            for (auto list : userCart)
+            {
+                auto specificItem = list.second;
+                itemsCarted += specificItem.getItemsInfo();
+            }
+        }
+        return itemsCarted + "\nTotal price: " + std::to_string(getCartTotal()).substr(0, 4) + "\n";
+    }
+
+    double getCartTotal()
+    {
+        double totalSum = 0;
+        for (auto list : userCart)
+        {
+            auto specificItem = list.second;
+            totalSum += specificItem.getItemPrice();
+        }
+        return totalSum;
+    }
+    bool isEmpty()
+    {
+        return userCart.empty();
+    }
+};
---
+++ shoppingCart.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include "objects.h"
+
+std::vector<Product> allItems = {
+    Product(1, "Cookies", 3.99),
+    Product(2, "Milk", 4.99),
+    Product(3, "KitKat", 1.99),
+    Product(4, "Pizza", 12.99),
+    Product(5, "Burger", 4.99),
+    Product(6, "Taco", 2.99),
+};
+
+Product addProduct()
+{
+    std::cout << "The available products are listed below: \n";
+
+    for (auto prod : allItems)
+    {
+        std::cout << "(" << prod.getFirstLetter() << ")" << prod.getDisplayName().substr(1);
+    }
+
+    // Make a valid choice to add item to cart
+    std::cout << "------------**------------\n";
+    char choice;
+    std::cout << "Please enter a choice: ";
+    std::cin >> choice;
+    while (!(choice == 'C' || choice == 'M' || choice == 'K' || choice == 'P' || choice == 'B' || choice == 'T'))
+    {
+        std::cout << "\n------Please make a valid choice.------\n\n";
+        std::cout << "Please enter a choice: ";
+        std::cin >> choice;
+    }
+    std::cout << "------------**------------\n";
+
+    for (auto prod : allItems)
+    {
+        if (choice == prod.getFirstLetter())
+        {
+            return prod;
+        }
+    }
+    return Product(7, "TEST", 0);
+}
+
+int main()
+{
+    char choice;
+    Cart cart;
+    // Take in input from user
+    while (true)
+    {
+        std::cout << "Make a choice: (a)dd item | (v)iew cart | (c)heckout: ";
+        std::cin >> choice;
+
+        // Validate user choice and accordingly show data
+        if (choice == 'a' || choice == 'v' || choice == 'c')
+        {
+            if (choice == 'a')
+            {
+                // Show list of items
+                Product userChose = addProduct();
+
+                std::cout << "\nAdded to the cart: "
+                          << userChose.getItemName() << "\n"
+                          << std::endl;
+                // Add mentioned item to cart
+                cart.addProduct(userChose);
+            }
+            else if (choice == 'v')
+            {
+                // Show the cart
+                std::cout << "------------**------------\n";
+                std::cout << cart.viewCart();
+                std::cout << "------------**------------\n";
+            }
+            else
+            {
+                // Prompt user to checkout with Cash
+                break;
+            }
+        }
+        else
+        {
+            std::cout << "\n------Please make a valid choice.------\n\n";
+        }
+    }
+
+    return 0;
+}
